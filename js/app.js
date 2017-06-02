b703d37fd82791f03919604d0deba7ed6791f665
mellaniesoriano <msoriano808@gmail.com>
2017-06-01 22:52:55 -1000

    btn7 done

--- js/app.js
+++ js/app.js
@@ -140,7 +140,7 @@
 
    btn6.addEventListener('click', function() {
     setMyLightGreen.call(btn6);
-   })
+   });
 
 
   /*
@@ -151,6 +151,10 @@
    * using the Function prototype method: call
    * to set the context to the correct object
    */
+
+   btn7.addEventListener('click', () => {
+    setMyLightGreen.call(btn7);
+   })
 
 
   /*
